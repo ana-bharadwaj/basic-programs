55e20ce00dae619cf08cc18dd4f3436269446585
Anagha Bharadwaj <anaghabharadwaj02@gmail.com>
2021-06-03 12:03:50 +0530

    frequency using hashmap

--- src/FrequencyOfWords.java
+++ src/FrequencyOfWords.java
@@ -1,25 +1,27 @@
 /**
  * Find the frequency of the words existing in a given string
- *
+ * <p>
  * Example 1:
  * string s = " My name is foo"
  * output:
- *      My : 1
- *      name : 1
- *      is : 1
- *      foo : 1
- *
+ * My : 1
+ * name : 1
+ * is : 1
+ * foo : 1
+ * <p>
  * Example 2:
  * string s = "I love coding because coding is cool and coding is great"
- *      I : 1
- *      love : 1
- *      coding : 3
- *      because : 1
- *      is : 2
- *      cool : 1
- *      and : 1
- *      great : 1
+ * I : 1
+ * love : 1
+ * coding : 3
+ * because : 1
+ * is : 2
+ * cool : 1
+ * and : 1
+ * great : 1
  */
+
+import java.util.HashMap;
 
 public class FrequencyOfWords {
     public static void main(String[] args) {
@@ -28,8 +30,22 @@ public class FrequencyOfWords {
         freq.CountWords("I love coding because coding is cool and coding is great");
     }
 
-    public void CountWords(String s){
-        // Your code goes here
-    }
+    public void CountWords(String s) {
+        String[] words = s.split("\\s");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (String w : words) {
 
+            if (!map.containsKey(w)) {
+                map.put(w, 1);
+            } else {
+                int ke = map.get(w);
+                map.put(w, ke + 1);
+            }
+
+        }
+        System.out.println(map);
+    }
 }
+
+
+
