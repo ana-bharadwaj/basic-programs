5d9ff9cd3c4f94b426c2a8582a0d97cec09df499
Tejus Bharadwaj <tejusbharadwaj@gmail.com>
2021-06-10 15:22:17 +0530

    Reverse an integer

---
+++ src/ReverseInt.java
@@ -0,0 +1,12 @@
+public class ReverseInt {
+    public static void main(String[] args) {
+        ReverseInt reverseInt = new ReverseInt();
+        System.out.println("The reverse of 1234 is: "+ reverseInt.reverse(1234));
+        System.out.println("The reverse of 1234 is: "+ reverseInt.reverse(9877));
+    }
+    public int reverse(int num){
+        // Your code goes here...
+
+        return 0;
+    }
+}
